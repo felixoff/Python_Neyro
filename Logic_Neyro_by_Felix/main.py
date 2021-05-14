735664c87035f04d4deb14f042b3b212f8daef26
Nikita <N.Felixoff@yandex.ru>
2021-05-14 23:26:53 +0300

    neyron

---
+++ Logic_Neyro_by_Felix/main.py
@@ -0,0 +1,146 @@
+import numpy as np
+
+
+def sigmoid(x):
+    # Функция активации sigmoid:: f(x) = 1 / (1 + e^(-x))
+    return 1 / (1 + np.exp(-x))
+
+
+def deriv_sigmoid(x):
+    # Производная от sigmoid: f'(x) = f(x) * (1 - f(x))
+    return np.exp(-x) / (1 + np.exp(-x)) / (1 + np.exp(-x))
+
+
+def mse_loss(y_true, y_pred):
+    # y_true и y_pred являются массивами numpy с одинаковой длиной
+    return ((y_true - y_pred) ** 2).mean()
+
+
+class OurNeuralNetwork:
+    """
+    Нейронная сеть, у которой:
+        - 2 входа
+        - скрытый слой с двумя нейронами (h1, h2)
+        - слой вывода с одним нейроном (o1)
+    """
+
+    def __init__(self):
+        # Вес
+        self.w1 = np.random.normal()
+        self.w2 = np.random.normal()
+        self.w3 = np.random.normal()
+        self.w4 = np.random.normal()
+        self.w5 = np.random.normal()
+        self.w6 = np.random.normal()
+
+        # Смещения
+        self.b1 = np.random.normal()
+        self.b2 = np.random.normal()
+        self.b3 = np.random.normal()
+
+    def feedforward(self, x):
+        # x является массивом numpy с двумя элементами
+        h1 = sigmoid(self.w1 * x[0] + self.w2 * x[1] + self.b1)
+        h2 = sigmoid(self.w3 * x[0] + self.w4 * x[1] + self.b2)
+        o1 = sigmoid(self.w5 * h1 + self.w6 * h2 + self.b3)
+        return o1
+
+    def train(self, data, all_y_trues):
+
+        learn_rate = 0.1
+        epochs = 1000  # количество циклов во всём наборе данных
+
+        for epoch in range(epochs):
+            for x, y_true in zip(data, all_y_trues):
+                # --- Выполняем обратную связь (нам понадобятся эти значения в дальнейшем)
+                sum_h1 = self.w1 * x[0] + self.w2 * x[1] + self.b1
+                h1 = sigmoid(sum_h1)
+
+                sum_h2 = self.w3 * x[0] + self.w4 * x[1] + self.b2
+                h2 = sigmoid(sum_h2)
+
+                sum_o1 = self.w5 * h1 + self.w6 * h2 + self.b3
+                o1 = sigmoid(sum_o1)
+                y_pred = o1
+
+                # --- Подсчет частных производных
+                # --- Наименование: d_L_d_w1 представляет "частично L / частично w1"
+                d_L_d_ypred = -2 * (y_true - y_pred)
+
+                # Нейрон o1
+                d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
+                d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
+                d_ypred_d_b3 = deriv_sigmoid(sum_o1)
+
+                d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
+                d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
+
+                # Нейрон h1
+                d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
+                d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
+                d_h1_d_b1 = deriv_sigmoid(sum_h1)
+
+                # Нейрон h2
+                d_h2_d_w3 = x[0] * deriv_sigmoid(sum_h2)
+                d_h2_d_w4 = x[1] * deriv_sigmoid(sum_h2)
+                d_h2_d_b2 = deriv_sigmoid(sum_h2)
+
+                # --- Обновляем вес и смещения
+                # Нейрон h1
+                self.w1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
+                self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
+                self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
+
+                # Нейрон h2
+                self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
+                self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
+                self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
+
+                # Нейрон o1
+                self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
+                self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
+                self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
+
+            # --- Подсчитываем общую потерю в конце каждой фазы
+            if epoch % 10 == 0:
+                y_preds = np.apply_along_axis(self.feedforward, 1, data)
+                loss = mse_loss(all_y_trues, y_preds)
+                print("Epoch %d loss: %.3f" % (epoch, loss))
+
+
+# Определение набора данных
+data = np.array([
+    [20.838, 45],
+    [100.2828, 78],
+    [4.8833, 120],
+    [3.888, 95],
+    [67.467, 111],
+    [13.589, 5],
+    [53.356, 57],
+    [68.111, 78],
+    [3.268, 100],
+    [45.874, 55],
+])
+
+all_y_trues = np.array([
+    0.04,
+    0.57,
+    0.2,
+    0.43,
+    0.19,
+    0.5,
+    0.7,
+    0.3,
+    0.36,
+    0.45
+])
+
+# Тренируем нашу нейронную сеть!
+network = OurNeuralNetwork()
+network.train(data, all_y_trues)
+
+# добавим данные по еще двум участкам и найдем кин для них
+obj11 = np.array([54.209, 46])
+obj12 = np.array([23.450, 78])
+print("obj11: %.3f" % network.feedforward(obj11))  # 0.464
+print("obj12: %.3f" % network.feedforward(obj12))  # 0.310
